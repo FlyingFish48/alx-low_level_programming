b5aa9904a975cf5d314b584ad99c0fa27b95b7e8
Ashley <ashleymukuku48@gmail.com>
2023-07-13 14:02:42 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+
 /**
- * Main - Entry point
+ * main - Entry point
+ *
+ * Description: prints "Programming is like a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
